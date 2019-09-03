3bf850d6e9bee88c52f3cdfcd1080fc4b1f61cf2
Ibrahim Numanagić <ibrahimpasa@gmail.com>
2019-09-02 17:14:08 -0700

    Add final tests

---
+++ aldy/tests/test_paper.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# 786
+
+# Aldy source: test_cn.py
+#   This file is subject to the terms and conditions defined in
+#   file 'LICENSE', which is part of this source code package.
+
+
+from nose.tools import *
+import unittest
+import logbook.more
+import ast
+import os 
+
+import aldy.genotype
+from aldy.common import script_path
+
+
+def solve(data, path, profile, reference):
+   sols = aldy.genotype.genotype(
+      'cyp2d6', 
+      sam_path=path,
+      profile=profile,
+      output_file=None, 
+      reference=reference)
+   assert_equal(data, [s.diplotype for s in sols])
+
+
+def test_pgx1_cyp2d6():
+   path = os.environ.get('ALDY_INTERNAL_SAMPLES', '.')
+   path = f'{path}/cdc/pgrnseq-v1/bams'
+   with open(script_path('aldy.tests.paper/data-pgx1.json')) as f:
+      data = f.read()
+      data = ast.literal_eval(data)
+   for file in data:
+      yield (solve,
+             data[file],
+             f'{path}/{file}.cram',
+             'pgrnseq-v1',
+             f'{path}/cram-genome.fa')
+
+
+def test_pgx2_cyp2d6():
+   path = os.environ.get('ALDY_INTERNAL_SAMPLES', '.')
+   path = f'{path}/baylor/pgrnseq-v2/bams'
+   with open(script_path('aldy.tests.paper/data-pgx2.json')) as f:
+      data = f.read()
+      data = ast.literal_eval(data)
+   for file in data:
+      yield (solve,
+             data[file],
+             f'{path}/{file}.cram',
+             'pgrnseq-v2',
+             f'{path}/cram-genome.fa')
+
+
+def test_illumina_cyp2d6():
+   path = os.environ.get('ALDY_INTERNAL_SAMPLES', '.')
+   path = f'{path}/1000genomes-illumina/bams'
+   with open(script_path('aldy.tests.paper/data-illumina.json')) as f:
+      data = f.read()
+      data = ast.literal_eval(data)
+   for file in data:
+      yield (solve,
+             data[file],
+             f'{path}/{file}.bam',
+             'illumina',
+             f'{path}/cram-genome.fa')
